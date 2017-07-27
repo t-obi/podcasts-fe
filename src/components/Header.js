3055760b56a65e139b580b8d281320565ea0ae8d
Tobias Kloht <mail@kloht.com>
2017-07-27 15:28:57 +0200

    add subscription-page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,8 @@ function Header(props) {
   return (
     <header className="bg-dark-red w-100 ph3 pv3 pv4-ns ph4-m ph4-l z-1 top-0">
       <nav className="f6 fw6 ttu tracked">
-        <Link className="link grow white dib" to="/">Podcasts</Link>
+        <Link className="link grow white dib f5" to="/">Podcasts</Link>
+        <Link className="link grow white dib ml4" to="/subscriptions">Subscriptions</Link>
         <UserMenu session={props.session}/>
       </nav>
     </header>
--- src/components/SubscriptionsPage.js
+++ src/components/SubscriptionsPage.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
+import Immutable from 'immutable';
+import { Arrow } from 'reline';
+import { Link } from 'react-router';
 
 import { jsonApi } from '../actions';
 
-class FeedDetailsPage extends Component {
+class SubscriptionsPage extends Component {
 
   async componentDidMount() {
     const {
@@ -15,6 +18,22 @@ class FeedDetailsPage extends Component {
     handleJsonApiResponse(result.data);
   }
 
+  componentDidUpdate(prevProps) {
+    if(prevProps.subscriptions !== this.props.subscriptions) {
+      const missingFeeds = this.props.subscriptions.toList()
+        .filter(x => {
+          const feedId = x.get('feed-id');
+          return !(this.props.subscribedFeeds && this.props.subscribedFeeds.has(feedId));
+        })
+        .map(x => x.get('feed-id'));
+      console.log('missing feeds: ', missingFeeds.toJS());
+      missingFeeds.forEach(async x => {
+        const result = await axios.get(`feeds/${x}`);
+        this.props.handleJsonApiResponse(result.data);
+      })
+    }
+  }
+
   render() {
 
     if(!this.props.subscriptions) {
@@ -22,18 +41,39 @@ class FeedDetailsPage extends Component {
         <div>Loading Subscriptions...</div>
       );
     }
-
     return (
       <div>
-        {this.props.subscriptions.map(x => <div>Subscription: {x}</div>)}
+        {this.props.subscribedFeeds.map((x, key) => <div className="ma3 ba b--dotted pa3">
+          {!x
+            ? <div> Loading Details...</div>
+            : <div className="cf">
+                <h4 className="mt0 h2 flex items-center">
+                  <Link to={`feeds/${x.get('feed-id')}`}>{x.get('title')}</Link>
+                  <a href={x.get('link')}>
+                    <Arrow className="h-100 ml2 light-red dim" />
+                  </a>
+                </h4>
+                <img src={x.get('image-url')}
+                  alt=""
+                  className="h4 w4 fl mr2 mb2"
+                />
+                <div>{x.get('description')}</div>
+              </div>
+          }
+        </div>)}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => {
+  const subscriptions = state.api.getIn(['subscription'], new Immutable.List());
   return {
-    subscriptions: state.api.getIn(['subscription']),
+    subscriptions: subscriptions,
+    subscribedFeeds: subscriptions.map((x) =>
+      state.api.getIn(['feed', '' + x.get('feed-id')], new Immutable.Map())
+      .set('feed-id', x.get('feed-id'))
+    ),
   }
 }
 
@@ -45,4 +85,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(FeedDetailsPage);
+export default connect(mapStateToProps, mapDispatchToProps)(SubscriptionsPage);
